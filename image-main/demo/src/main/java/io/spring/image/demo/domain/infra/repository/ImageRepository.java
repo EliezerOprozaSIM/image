06953bf1c1dac889f6e366049058fee94b6fd1e5
EliezeR.Lerzer.prozaaa <eliezer.o@estudante.ifmt.edu.br>
2026-04-17 13:25:28 -0400

    tabela e repositório de imagem

---
+++ image-main/demo/src/main/java/io/spring/image/demo/domain/infra/repository/ImageRepository.java
@@ -0,0 +1,6 @@
+package io.spring.image.demo.domain.infra.repository;
+import io.spring.image.demo.domain.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ImageRepository  extends JpaRepository<Image, String> {
+
+}
